936cc61e39912745a98ac69216263523e722a8bd
eliascharadia <eliascharadia01@gmail.com>
2026-06-12 11:54:38 -0300

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('ELIAS-CHARADIA-TP2-PROG4-2026-C1-FRONTEND');
+}
