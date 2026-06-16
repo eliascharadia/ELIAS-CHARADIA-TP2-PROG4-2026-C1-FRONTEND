f1edd01d93d216181be25b4adac3789fc49acb89
eliascharadia <eliascharadia01@gmail.com>
2026-06-16 11:21:21 -0300

    feat: borre por defecto de angular y agregue componentes login y registro al html y rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'registro',
+        loadComponent: () =>
+            import('./components/registro/registro')
+                .then(m => m.Registro)
+    },
+    {
+        path: '',
+        loadComponent: () =>
+            import('./components/login/login')
+                .then(m => m.Login)
+    }
+];
