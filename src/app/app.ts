2d662f0c7feb099c30eafc5f67f829c3c3dcbb76
eliascharadia <eliascharadia01@gmail.com>
2026-06-30 00:44:41 -0300

    add: componente para mostrar una pantalla de cargando

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,33 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router, RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Modal } from './components/modal/modal';
+import { Cargando } from './components/cargando/cargando';
+import { CargandoService } from './services/cargando.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Modal],
+  imports: [RouterOutlet, Header, Modal, Cargando],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('ELIAS-CHARADIA-TP2-PROG4-2026-C1-FRONTEND');
+
+   private router = inject(Router);
+  private loaderService = inject(CargandoService);
+
+  ngOnInit(): void {
+    this.router.events.subscribe((evento) => {
+      if (evento instanceof NavigationStart) {
+        this.loaderService.mostrar();
+      } else if (
+        evento instanceof NavigationEnd ||
+        evento instanceof NavigationCancel ||
+        evento instanceof NavigationError
+      ) {
+        this.loaderService.ocultar();
+      }
+    });
+  }
 }
