9bd63cd50b1d68fa2962aa0d07f1b63fb21fb502
eliascharadia <eliascharadia01@gmail.com>
2026-06-30 19:47:01 -0300

    feat: ahora el header muestra distiontos botones dependiendo del perfil del usuario

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject, computed } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Autenticacion } from '../../services/autenticacion';
+import { SesionService } from '../../services/sesion.service';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {}
+export class Header {
+  authService = inject(Autenticacion);
+  private sesionService = inject(SesionService);
+
+  cerrarSesion(): void {
+    this.sesionService.cerrarSesion();
+  }
+}
